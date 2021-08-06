b2986c622ffb3874687927d54c5f58c918bd68ba
Andy Whetstone <andy_whetstone@live.com>
2021-08-06 17:41:15 -0500

    Add functionality to calculator buttons

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+let firstOperand = '';
+let secondOperand = '';
+
+
+const numberButtons = document.querySelectorAll('.number-button');
+const operatorButtons = document.querySelectorAll('.operator-button');
+const clearButton = document.getElementById('clear-button');
+const deleteButton = document.getElementById('delete-button');
+const currentScreen = document.querySelector('.screen-current');
+
+currentScreen.textContent = '0';
+
+numberButtons.forEach((button => 
+    button.addEventListener('click', () => appendNumberToScreen(button.textContent)))
+);
+
+operatorButtons.forEach((button => 
+    button.addEventListener('click', () => appendNumberToScreen(button.textContent)))
+);
+
+clearButton.addEventListener('click', () => clearScreen());
+deleteButton.addEventListener('click', () => {
+    currentScreen.textContent = currentScreen.textContent.slice(0,-1)
+    if(currentScreen.textContent == ''){
+        currentScreen.textContent = '0';
+    }
+});
+
+function appendNumberToScreen(number) {
+    if(currentScreen.textContent == '0') currentScreen.textContent = '';
+    currentScreen.textContent += number;
+}
+
+function operate() {
+    
+}
+
+function clearScreen() {
+    currentScreen.textContent = '0';
+}
+
 function add(num1, num2){
     return num1 + num2;
 }
